cb6580d704b093268ed0d9368e709aa1f0b97755
brk-a <fnjakai@outlook.com>
2022-04-27 01:27:53 -0700

    Task 3 & 4: try v 1.3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,6 +11,23 @@ from typing import Union, Callable
 from functools import wraps
 
 
+def replay(method: Callable) -> None:
+    """displays the history of calls of a particular function."""
+    redis = method.__self__._redis
+    qualified_name = method.__qualname__
+    num_of_calls = redis.get(qualified_name).decode("utf-8")
+    print("{} was called {} times:".format(qualified_name, num_of_calls))
+    input_key = qualified_name + ":inputs"
+    output_key = qualified_name + ":outputs"
+    input_list = redis.lrange(input_key, 0, -1)
+    output_list = redis.lrange(output_key, 0, -1)
+    r_zipped = list(zip(input_list, output_list))
+    for key, value in r_zipped:
+        key = key.decode("utf-8")
+        value = value.decode("utf-8")
+        print("{}(*{}) -> {}".format(qualified_name, key, value))
+
+
 class Cache:
     """defines methods that perform few common redis-py operations
        on redis
